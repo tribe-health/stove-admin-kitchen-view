a2ac399466473b79d1cc77852d0a679e645815bd
Travis James <travis@tribehealthsolutions.com>
2025-04-02 16:01:28 -0500

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import * as ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(
+  React.createElement(React.StrictMode, null, 
+    React.createElement(App, null)
+  )
+)
